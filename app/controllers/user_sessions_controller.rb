a5c2976daa68fea860adf1d84fc6901c755b04df
shibamon48 <eaglel4rk@gmail.com>
2024-09-03 15:43:46 +0900

    ユーザー登録、ログインログアウト機能実装

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,22 @@
+class UserSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      flash[:success] = 'Login successful'
+      redirect_to :map
+    else
+      flash.now[:error] = 'Login failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    flash[:error] = "ログアウトしました!"
+    redirect_to root_path, status: :see_other
+  end
+end
