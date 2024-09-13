bbb8bbde78bfb32f1e2e4214ca9dc5bc89afff76
shibamon48 <eaglel4rk@gmail.com>
2024-09-13 19:07:53 +0900

    ルートの編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[new edit create update destroy]
   root "tops#index"
 
-  resource :route, only: %i[new index create]
+  resource :route, only: %i[new index create destroy]
 
   get "login" => "user_sessions#new", :as => :login
   post "login" => "user_sessions#create"
